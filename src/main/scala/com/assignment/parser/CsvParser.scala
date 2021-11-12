c5a1d6787fb245dff19cc1bf7c0e49cccf1f8226
CedricSamboun <72805419+CedricSamboun17@users.noreply.github.com>
2021-11-12 18:15:55 +0100

    Add files via upload

---
+++ src/main/scala/com/assignment/parser/CsvParser.scala
@@ -0,0 +1,17 @@
+package com.assignment.parser
+
+import com.github.tototoshi.csv.CSVReader
+
+import java.io.File
+
+
+abstract class CsvParser[T] {
+
+  def parse(filename: String): List[T] = {
+    val reader = CSVReader.open(new File(filename))
+    val data = reader.allWithHeaders()
+    write(data)
+  }
+
+  def write(data: List[Map[String, String]]): List[T]
+}
